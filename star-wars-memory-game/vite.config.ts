d10e8b87ef5d52825d82d1e82345b580858ec3df
Lucas Saliba <lucassaliba10@gmail.com>
2022-02-04 09:59:44 -0300

    retry build

--- star-wars-memory-game/vite.config.ts
+++ star-wars-memory-game/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `margin: 0;  padding: 0; box-sizing: border-box;  text-decoration: none;`
+      }
+    }
+  }
+});
